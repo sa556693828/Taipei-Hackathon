a5f096ee0a7da5ef6207e67614d06dec44455776
sa556693828 <sa556693828@gmail.com>
2023-05-18 17:18:48 +0800

    fav

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import theme from "@/components/theme";
 import Icons from "@/components/icons/Icons";
+import Logo from "../../public/Favicon.png";
 
 const emotionCache = createCache({
   key: "style",
@@ -16,19 +17,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href={
-            "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          }
-          rel="stylesheet"
-        />
         <title>Taipei Hackathon</title>
+        <link rel="icon" href={Logo.src} />
       </Head>
       <CacheProvider value={emotionCache}>
         <ChakraProvider theme={theme}>
